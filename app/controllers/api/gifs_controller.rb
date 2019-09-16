11dc9048a23f681d0dc939567996c74593fa33f6
eddie <eddielee001@gmail.com>
2019-09-16 13:32:51 -0500

    adds figaro gem

--- app/controllers/api/gifs_controller.rb
+++ app/controllers/api/gifs_controller.rb
@@ -2,7 +2,11 @@ class Api::GifsController < ApplicationController
   def index
     # @gifs = HTTP.get("https://api.giphy.com/v1/gifs/search?api_key=USiosdTm2ZGXVCfcwCQvqyReX6zpwCU8&q=#{params[:search]}&limit=25&offset=0&rating=G&lang=en").parse["data"]
     # render 'index.json.jb'
+
     @gifs = HTTP.auth("Bearer USiosdTm2ZGXVCfcwCQvqyReX6zpwCU8").get("https://api.giphy.com/v1/gifs/search?api_key=USiosdTm2ZGXVCfcwCQvqyReX6zpwCU8&q=#{params[:search]}&limit=25&offset=0&rating=G&lang=en").parse["data"]
     render 'index.json.jb'
+
+    # @gifs = HTTP.headers("Authorization" => "Bearer #{ENV['GIPHY_API_KEY']}").get("https://api.giphy.com/v1/gifs/search?q=#{params[:search]}&limit=25&offset=0&rating=G&lang=en").parse["data"]
+    # render 'index.json.jb'
   end
 end
